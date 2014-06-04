ccb5971815c9200ce90412794a2771661e5af92c
Martin Lindhe <martin.lindhe@freespee.com>
2014-06-04 16:19:22 +0200

    composer: TCPDF does support autoloading, fixed

--- class/Writer/Barcode2D/Qrcode.php
+++ class/Writer/Barcode2D/Qrcode.php
@@ -1,8 +1,6 @@
 <?php
 namespace Writer\Barcode2D;
 
-require_once realpath(__DIR__.'/../../../vendor/tecnick.com/tcpdf').'/tcpdf_barcodes_2d.php';
-
 class Qrcode extends \Writer\Barcode2D
 {
     public function renderAsHtml($code)
