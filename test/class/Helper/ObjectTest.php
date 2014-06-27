58c3521e34308b3bd8676f167f7584d05aa030c8
Martin Lindhe <martin@ubique.se>
2014-06-27 22:55:01 +0200

    \Helper\Object::describePropertiesWithValues() added

---
+++ test/class/Helper/ObjectTest.php
@@ -0,0 +1,43 @@
+<?php
+
+class MyClass
+{
+    var $prop1;
+    var $prop2;
+    var $prop3;
+    private $privProp;
+
+    public function setPrivProp($s)
+    {
+        $this->privProp = $s;
+    }
+}
+
+/**
+ * @group Helper
+ */
+class ObjectTest extends \PHPUnit_Framework_TestCase
+{
+
+    function testBasic()
+    {
+        $o = new MyClass();
+        $o->prop1 = "hej";
+        $o->prop2 = ''; // verify empty props are not shown
+        $o->prop3 = "hallå";
+        $o->setPrivProp("bah"); // verify private props are not shown
+        $this->assertEquals(
+            "prop1: hej\n".
+            "prop3: hallå",
+            \Helper\Object::describePropertiesWithValues($o)
+        );
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    function testBadInput()
+    {
+        \Helper\Object::describePropertiesWithValues("hej");
+    }
+}
