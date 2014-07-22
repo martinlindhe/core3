aeb916dc379a41efa1d523d88107fc673bdad77e
Martin Lindhe <martin.lindhe@freespee.com>
2014-07-22 09:20:26 +0200

    lint: break long lines

--- class/Writer/Uuid.php
+++ class/Writer/Uuid.php
@@ -56,11 +56,18 @@ class Uuid
     {
         return sprintf(
             '%08s-%04s-%04x-%04x-%12s',
-            substr($hash, 0, 8), // 32 bits "time_low"
-            substr($hash, 8, 4), // 16 bits "time_mid"
-            (hexdec(substr($hash, 12, 4)) & 0x0fff) | $version, // 16 bits "time_hi_and_version", four most significant bits holds version number
-            (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000, // 16 bits, 8 bits "clk_seq_hi_res", 8 bits "clk_seq_low", two most significant bits holds zero and one for variant DCE1.1
-            substr($hash, 20, 12)                             // 48 bits "node"
+            // 32 bits "time_low"
+            substr($hash, 0, 8),
+            // 16 bits "time_mid"
+            substr($hash, 8, 4),
+            // 16 bits "time_hi_and_version",
+            // four most significant bits holds version number
+            (hexdec(substr($hash, 12, 4)) & 0x0fff) | $version,
+            // 16 bits, 8 bits "clk_seq_hi_res", 8 bits "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+            // 48 bits "node"
+            substr($hash, 20, 12)
         );
     }
 
